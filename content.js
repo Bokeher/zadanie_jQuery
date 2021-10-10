041e1d4a977e423ce01578098ea4de6e74475d33
Bokeher <bokeher47@gmail.com>
2021-10-10 19:05:36 +0200

    optimized deleting duplicates + added comments + changed variable names + added regEx + small fixes

--- content.js
+++ content.js
@@ -1,30 +1,38 @@
 var stNumb = ['st1', 'st8', 'st10', 'st13', 'st14', 'st7', 'st4', 'st11'];
-const klasa = "3 PTN";
-var foundData = [];
+const classSymbol = "3PTN";
+var data = [];
+var tempTeacher = "";
+var tempNumber = "";
 var temp = "";
-var temp2 = "";
 
+// add class temp to every needed element
 for(j=0; j<stNumb.length; j++){
     for(i=0; i<$(`.${stNumb[j]}`).length; i++) $(`.${stNumb[j]}:eq(${i})`).addClass("temp");
 }
 
 $('.temp').each(function(){
-    if($(this).text().search("2021") != -1) temp = $(this).text();
-    else if($(this).text().substring(1, 6) == klasa){
-        foundData.push(`<br><br><b>${temp}</b>`);
-        foundData.push(`<br>${temp2} | ${$(this).text()}`);
+    var classList = $(this).attr("class");
+    var classArr = classList.split(/\s+/);
+
+    if(classArr.includes("st1")) {
+        tempTeacher = $(this).text();
+        if(!tempTeacher.includes("2021")) temp = $(".st0").text();
+    }
+    else if($(this).text().replace(/\s/g, '').substring(0, classSymbol.length) == classSymbol){
+        data.push(`<br><br><b>${tempTeacher}</b>`);
+        data.push(`<br>${tempNumber} | ${$(this).text()}`);
     }
     
-    temp2 = $(this).text()
+    var tempData = $(this).text().replace(/\s/g, '');        // /\s/g   - all whitespaces 
+    if(tempData.match(/^\d+$/)) tempNumber = $(this).text(); // /^\d+$/ - entire String has only digits 
 });
 
-for(i=0; i<foundData.length-2 && foundData.length>2; i++){
-    if(foundData[i] == foundData[i+2]) foundData.splice(i+2, 1);
-}
+// delete duplicates (teachers)
+data = [...new Set(data)];
 
-if(foundData.length>0) {
-    foundData[0] = foundData[0].substring(8, foundData[0].length);
-    document.write(foundData.join(""));
+if(data.length>0) {
+    data[0] = data[0].substring(8, data[0].length);
+    document.write(`<strong>${temp}</strong><br><br>${data.join("")}`);
 }else document.write(`<h1>Brak zastępstw</h1>`);
 
 document.body.style.fontFamily = "Arial";
